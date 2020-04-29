86b943082165b1a4ea1b9546adf50dbbb34d9130
emorrison <emorrison@collegeboard.org>
2020-04-28 23:30:31 -0400

    feat(project): query hist table by date range script and appsync setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import { ApolloLink } from 'apollo-link'
+import { createHttpLink } from 'apollo-link-http'
+import ApolloClient from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createAuthLink } from 'aws-appsync-auth-link'
+import { createSubscriptionHandshakeLink } from 'aws-appsync-subscription-link'
+
 import { Router } from './routes/router'
 
 export const App: React.FC = () => <Router />
---
+++ scripts/query-specific-items.ts
@@ -0,0 +1,90 @@
+import aws from 'aws-sdk'
+import util from 'util'
+const region = 'us-east-1'
+
+/* use dev nonprod account */
+const profile = 'default'
+
+/* set credentials */
+const credentials = new aws.SharedIniFileCredentials({ profile })
+aws.config.update({ region })
+
+/* enable promises */
+aws.config.credentials = credentials
+
+/* create dynamo instance */
+const dynamoDb = new aws.DynamoDB.DocumentClient()
+
+/* define the table name */
+const TableName = 'metro-hist'
+
+type PrimaryKey = Record<'id' | 'archiveTime', string>
+
+/* sleep for x milliseconds */
+export const sleep = (seconds: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, seconds * 1000))
+
+/* determine if the key */
+const inspectKey = (key: PrimaryKey | undefined) =>
+  typeof key !== 'undefined' && !!key.id && !!key.archiveTime
+
+/* run the main function */
+const run = async (lastKey?: PrimaryKey, prevApiCount = 0): Promise<any> => {
+  /* check if the previous key exists */
+  const hasPreviousKey = inspectKey(<PrimaryKey>lastKey)
+
+  /* define the exclusive start key */
+  const startKey = hasPreviousKey ? { ExclusiveStartKey: lastKey } : {}
+
+  /* define the params object */
+  const Params = {
+    TableName,
+    KeyConditionExpression: '#pk = :pk AND #sk BETWEEN :date1 AND :date2',
+    ExpressionAttributeNames: { '#pk': 'id', '#sk': 'archiveTime' },
+    ExpressionAttributeValues: {
+      ':pk': 'api_count_history',
+      ':date1': '2020-04-28',
+      ':date2': '2020-04-29',
+    },
+    ...startKey,
+  }
+
+  /* query the table for api counts in the date range */
+  const results = await dynamoDb.query(Params).promise()
+
+  /* extract the items and the last key from the index query */
+  const { Items, LastEvaluatedKey } = results
+
+  /* log outcome */
+  if (LastEvaluatedKey) {
+    console.log('LastEvaluatedKey: ' + JSON.stringify(LastEvaluatedKey))
+    console.log(`Successfully queried ${Items?.length} items.`)
+  } else {
+    console.log('No LastEvaluatedKey.')
+    console.log(`Successfully queried ${Items?.length} items.`)
+  }
+
+  /* create a list of the org id event ids */
+  const apiCount =
+    Items?.reduce((store, { apiCount }) => store + apiCount, 0) ?? 0
+
+  console.log('apiCount:  ' + apiCount)
+
+  /* determine if there is a new last evaluated key */
+  const hasNewKey = inspectKey(<PrimaryKey>LastEvaluatedKey)
+
+  /* if the last evaluated key exists, */
+  if (hasNewKey) {
+    await sleep(3)
+
+    /* recursively run the function */
+    return run(<PrimaryKey>LastEvaluatedKey, prevApiCount + apiCount)
+  }
+
+  console.log('Total api calls made in date range: ', prevApiCount + apiCount)
+}
+
+/* execute program and catch errors */
+run()
+  .then(() => console.log('Done.'))
+  .catch((error: Error) => console.error(error))
